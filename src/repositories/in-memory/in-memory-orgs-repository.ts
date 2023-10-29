7717ffc9327484d2a6aeb08364e08f66ea56ec48
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-10-29 11:59:32 -0300

    feat: create orgs repositories in memory

---
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -0,0 +1,47 @@
+import { randomUUID } from 'node:crypto'
+import { Org, Prisma } from '@prisma/client'
+
+import { OrgsRepository } from '../orgs-repositories'
+
+export class InMemoryOrgsRepository implements OrgsRepository {
+  public items: Org[] = []
+
+  async create(data: Prisma.OrgCreateInput) {
+    const org = {
+      id: randomUUID(),
+      name: data.name,
+      admin_name: data.admin_name,
+      email: data.email,
+      password_hash: data.password_hash,
+      role: data.role ?? 'ADMIN',
+      cep: data.cep,
+      address: data.address,
+      whatsapp: data.whatsapp,
+      created_at: new Date(),
+    }
+
+    this.items.push(org)
+
+    return org
+  }
+
+  async findById(id: string) {
+    const org = this.items.find((item) => item.id === id)
+
+    if (!org) {
+      return null
+    }
+
+    return org
+  }
+
+  async findByEmail(email: string) {
+    const org = this.items.find((item) => item.email === email)
+
+    if (!org) {
+      return null
+    }
+
+    return org
+  }
+}
