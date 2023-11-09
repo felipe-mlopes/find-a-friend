572e495bb17c332faa87d87193661aa0615f27ec
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-09 06:45:21 -0300

    feat: add findByEmailOrName / change findByCity

--- src/repositories/orgs-repositories.ts
+++ src/repositories/orgs-repositories.ts
@@ -4,5 +4,6 @@ export interface OrgsRepository {
   create(data: Prisma.OrgCreateInput): Promise<Org>
   findById(id: string): Promise<Org | null>
   findByEmail(email: string): Promise<Org | null>
-  findByCity(city: string): Promise<Org[] | null>
+  findByEmailOrName(email: string, name: string): Promise<Org | null>
+  findByCity(city: string): Promise<Org[]>
 }
