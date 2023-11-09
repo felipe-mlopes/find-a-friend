8cc20d62db5fb8390cdcd7d167748594d3210dee
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-09 06:43:38 -0300

    feat: add migration to update pets table

---
+++ prisma/migrations/20231108103458_update_pet_table/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - Changed the type of `age` on the `pets` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `size` on the `pets` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `energy_level` on the `pets` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `independence_level` on the `pets` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `environment` on the `pets` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- CreateEnum
+CREATE TYPE "Age" AS ENUM ('PUPPY', 'ADULT', 'SENIOR');
+
+-- CreateEnum
+CREATE TYPE "EnergyLevel" AS ENUM ('CALM', 'PEACEFUL', 'FUSSY');
+
+-- CreateEnum
+CREATE TYPE "Size" AS ENUM ('SMALL', 'MEDIUM', 'BIG');
+
+-- CreateEnum
+CREATE TYPE "IndependenceLevel" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateEnum
+CREATE TYPE "Environment" AS ENUM ('TIGHT', 'NORMAL', 'WIDE');
+
+-- AlterTable
+ALTER TABLE "pets" DROP COLUMN "age",
+ADD COLUMN     "age" "Age" NOT NULL,
+DROP COLUMN "size",
+ADD COLUMN     "size" "Size" NOT NULL,
+DROP COLUMN "energy_level",
+ADD COLUMN     "energy_level" "EnergyLevel" NOT NULL,
+DROP COLUMN "independence_level",
+ADD COLUMN     "independence_level" "IndependenceLevel" NOT NULL,
+DROP COLUMN "environment",
+ADD COLUMN     "environment" "Environment" NOT NULL;
