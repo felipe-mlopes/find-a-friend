30fbdbe6d7236333a7f749b469bea89f525ffd92
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-15 17:43:56 -0300

    test: create unit test to search pets by char

---
+++ src/services/search-pets-by-characteristics.ts
@@ -0,0 +1,33 @@
+import { Pet } from '@prisma/client'
+import { PetQuery, PetsRepository } from '../repositories/pets-repositories'
+import { OrgsRepository } from '../repositories/orgs-repositories'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+type SearchPetsByCharacteristicsServiceRequest = PetQuery
+
+interface SearchPetsByCharacteristicsServiceResponse {
+  pets: Pet[]
+}
+
+export class SearchPetsByCharacteristicsService {
+  constructor(
+    private petsRepository: PetsRepository,
+    private orgsRepository: OrgsRepository,
+  ) {}
+
+  async execute(
+    query: SearchPetsByCharacteristicsServiceRequest,
+  ): Promise<SearchPetsByCharacteristicsServiceResponse> {
+    const orgs = await this.orgsRepository.findByCity(query.city)
+
+    if (orgs.length === 0) {
+      throw new ResourceNotFoundError()
+    }
+
+    const pets = await this.petsRepository.findManyByQuery(query)
+
+    return {
+      pets,
+    }
+  }
+}
