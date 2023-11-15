5fed3e8fdb74aa02be9a37825948eeb54d104cc0
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-15 19:41:51 -0300

    feat: create authenticate service

---
+++ src/services/authenticate.ts
@@ -0,0 +1,39 @@
+import { compare } from 'bcrypt'
+import { Org } from '@prisma/client'
+
+import { OrgsRepository } from '../repositories/orgs-repositories'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+interface AuthenticateServiceRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateServiceResponse {
+  org: Org
+}
+
+export class AuthenticateService {
+  constructor(private orgsRepository: OrgsRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateServiceRequest): Promise<AuthenticateServiceResponse> {
+    const org = await this.orgsRepository.findByEmail(email)
+
+    if (!org) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, org.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      org,
+    }
+  }
+}
