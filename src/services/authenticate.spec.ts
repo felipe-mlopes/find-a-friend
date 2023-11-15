bdb9fbb72bbe857d469f7fbe430a36a7363d350d
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-15 19:42:23 -0300

    test: create unit test authenticate service

---
+++ src/services/authenticate.spec.ts
@@ -0,0 +1,54 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { hash } from 'bcrypt'
+
+import { InMemoryOrgsRepository } from '../repositories/in-memory/in-memory-orgs-repository'
+import { AuthenticateService } from './authenticate'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+let orgsRepository: InMemoryOrgsRepository
+let sut: AuthenticateService
+
+describe('Authenticate Service', () => {
+  beforeEach(async () => {
+    orgsRepository = new InMemoryOrgsRepository()
+    sut = new AuthenticateService(orgsRepository)
+
+    await orgsRepository.create({
+      name: 'Org',
+      admin_name: 'John Doe',
+      email: 'org@example.com',
+      password_hash: await hash('123456', 6),
+      address: 'Example St',
+      cep: '21220000',
+      city: 'Rio de Janeiro',
+      whatsapp: '21912345678',
+    })
+  })
+
+  it('should be able to authenticate', async () => {
+    const { org } = await sut.execute({
+      email: 'org@example.com',
+      password: '123456',
+    })
+
+    expect(org.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    await expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    await expect(() =>
+      sut.execute({
+        email: 'org@example.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
