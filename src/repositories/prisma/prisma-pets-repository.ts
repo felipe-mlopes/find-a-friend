1a1cfe4562beef415eacd1f277f8f31286eab554
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-15 12:37:39 -0300

    feat: create Prisma Pets Repository

---
+++ src/repositories/prisma/prisma-pets-repository.ts
@@ -0,0 +1,85 @@
+import {
+  Org,
+  Prisma,
+  Age,
+  Size,
+  EnergyLevel,
+  IndependenceLevel,
+  Environment,
+} from '@prisma/client'
+import { PetQuery, PetsRepository } from '../pets-repositories'
+import { prisma } from '../../lib/prisma'
+
+interface SearchPetProps {
+  age?: Age
+  size?: Size
+  energyLevel?: EnergyLevel
+  independenceLevel?: IndependenceLevel
+  environment?: Environment
+}
+
+export class PrismaPetsRepository implements PetsRepository {
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet = await prisma.pet.create({
+      data,
+    })
+
+    return pet
+  }
+
+  async findById(id: string) {
+    const pet = await prisma.pet.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return pet
+  }
+
+  async findManyOrgs(orgs: Org[]) {
+    const orgsIdArray = orgs.map((org) => org.id)
+
+    const pets = prisma.pet.findMany({
+      where: {
+        org_id: {
+          in: orgsIdArray,
+        },
+      },
+    })
+
+    return pets
+  }
+
+  async findManyByQuery({
+    city,
+    age,
+    size,
+    energyLevel,
+    independenceLevel,
+    environment,
+  }: PetQuery) {
+    const query: SearchPetProps = {}
+
+    if (age !== null) query.age = age
+
+    if (size !== null) query.size = size
+
+    if (energyLevel !== null) query.energyLevel = energyLevel
+
+    if (independenceLevel !== null) query.independenceLevel = independenceLevel
+
+    if (environment !== null) query.environment = environment
+
+    const pets = await prisma.pet.findMany({
+      where: {
+        org: {
+          city,
+        },
+        ...query,
+      },
+    })
+
+    return pets
+  }
+}
