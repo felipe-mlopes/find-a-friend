c76d9ca6bb6473cf3a88d06ca48fa77f69cf1dd9
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-16 07:27:58 -0300

    feat: create get specific pet details service

---
+++ src/services/get-specific-pet-details.ts
@@ -0,0 +1,30 @@
+import { Pet } from '@prisma/client'
+
+import { PetsRepository } from '../repositories/pets-repositories'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface GetSpecificPetDetailsServiceRequest {
+  petId: string
+}
+
+interface GetSpecificPetDetailsServiceResponse {
+  pet: Pet
+}
+
+export class GetSpecificPetDetailsService {
+  constructor(private petsRepository: PetsRepository) {}
+
+  async execute({
+    petId,
+  }: GetSpecificPetDetailsServiceRequest): Promise<GetSpecificPetDetailsServiceResponse> {
+    const pet = await this.petsRepository.findById(petId)
+
+    if (!pet) {
+      throw new ResourceNotFoundError()
+    }
+
+    return {
+      pet,
+    }
+  }
+}
