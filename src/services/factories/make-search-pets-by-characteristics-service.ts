bb5bb5300f869f52fa0d4dbdff7f3bd5b49eac16
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-18 11:02:24 -0300

    feat: implement factorie service to search pets

---
+++ src/services/factories/make-search-pets-by-characteristics-service.ts
@@ -0,0 +1,15 @@
+import { PrismaOrgsRepository } from '../../repositories/prisma/prisma-orgs-repository'
+import { PrismaPetsRepository } from '../../repositories/prisma/prisma-pets-repository'
+import { SearchPetsByCharacteristicsService } from '../search-pets-by-characteristics'
+
+export function MakeSearchPetsByCharacteristicsService() {
+  const petsRepository = new PrismaPetsRepository()
+  const orgsRepository = new PrismaOrgsRepository()
+
+  const service = new SearchPetsByCharacteristicsService(
+    petsRepository,
+    orgsRepository,
+  )
+
+  return service
+}
