3f99dff16bd6ea7b58d6d57ae616c4029123b270
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-18 12:40:56 -0300

    feat: add fastifyJWT & pets routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,30 @@
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+
 import { ZodError } from 'zod'
 
 import { orgsRoutes } from './http/controllers/orgs/routes'
 import { env } from '../src/env'
+import { petsRoutes } from './http/controllers/pets/routes'
 
 export const app = fastify()
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
+
 app.register(orgsRoutes)
+app.register(petsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
