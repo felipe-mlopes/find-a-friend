22ae91d8e12fb8931694ae79d239aee38e3712cf
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-18 11:05:55 -0300

    feat: add session & token/refresh routes

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance } from 'fastify'
+
 import { create } from './create'
+import { authenticate } from './authenticate'
+import { refresh } from './refresh'
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post('/orgs', create)
+  app.post('/session', authenticate)
+
+  app.patch('/token/refresh', refresh)
 }
