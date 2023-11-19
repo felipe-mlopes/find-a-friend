bd2ed651bfef6abb3f80f32698d666d4e69ad8fe
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-19 12:49:53 -0300

    test: create get pet details e2e test

---
+++ src/http/controllers/pets/details.spec.ts
@@ -0,0 +1,55 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { Pet } from '@prisma/client'
+
+import { app } from '@/app'
+import { createAndAuthenticateOrg } from '@/utils/test/create-and-authenticate-org'
+import { prisma } from '@/lib/prisma'
+
+interface TokenProps {
+  sub: string
+  iat: number
+  exp: number
+}
+
+describe('Get Pet Details (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get a specific pet details', async () => {
+    const { token } = await createAndAuthenticateOrg(app)
+    const { sub } = token as TokenProps
+
+    /* const org = await prisma.org.findFirstOrThrow() */
+
+    const newPet = await request(app.server)
+      .post('/pets/create')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Paçoca',
+        description: '',
+        age: 'PUPPY',
+        energyLevel: 'FUSSY',
+        size: 'MEDIUM',
+        independenceLevel: 'MEDIUM',
+        environment: 'NORMAL',
+        images: [''],
+        requirement: [''],
+        orgId: sub,
+      })
+
+    /* const newPet = await prisma.pet.findFirst() */
+
+    const { id } = newPet.body.newPet.pet as Pet
+
+    const response = await request(app.server).get(`/pets/pet/${id}`).send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.pet.name).toEqual('Paçoca')
+  })
+})
