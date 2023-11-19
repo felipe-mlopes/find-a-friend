e1525fa735626fbcaaf6968f0d69e079da187e6d
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-19 12:45:21 -0300

    feat: change create and details routes

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,15 +1,16 @@
 import { FastifyInstance } from 'fastify'
 
-import { verifyJWT } from '../../middlewares/verify-jwt'
-import { register } from './register'
 import { searchByCity } from './search-by-city'
 import { searchByCharacteristics } from './search-by-characteritics'
 import { details } from './details'
+import { create } from './create'
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.get('/pets/:city', searchByCity)
   app.get('/pets/search/:city', searchByCharacteristics)
-  app.get('/pet/:id', details)
+  app.get('/pets/pet/:id', details)
 
-  app.post('/register-pet', { onRequest: [verifyJWT] }, register)
+  app.post('/pets/create', { onRequest: [verifyJWT] }, create)
 }
