e525d11398c0629f1fb132adc345c66ab5a1bc2f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-30 10:33:09 -0300

    test: change token to access_token, energyLevel to energy_level, independenceLevel to independence_level and sequency in newPet request and switch id to petId in newPet body and response route

--- src/http/controllers/pets/get-pet-details-by-id.controller.spec.ts
+++ src/http/controllers/pets/get-pet-details-by-id.controller.spec.ts
@@ -1,6 +1,5 @@
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import request from 'supertest'
-import { Pet } from '@prisma/client'
 
 import { app } from '@/app'
 import { createAndAuthenticateOrg } from '@/utils/test/create-and-authenticate-org'
@@ -16,28 +15,28 @@ describe('Get Pet Details (e2e)', () => {
   })
 
   it('should be able to get a specific pet details', async () => {
-    const { token } = await createAndAuthenticateOrg(app)
-    const { sub } = token as TokenProps
+    const { access_token } = await createAndAuthenticateOrg(app)
+    const { sub } = access_token as TokenProps
 
     const newPet = await request(app.server)
       .post('/pets/create')
-      .set('Authorization', `Bearer ${token}`)
+      .set('Authorization', `Bearer ${access_token}`)
       .send({
         name: 'Paçoca',
         description: '',
         age: 'PUPPY',
-        energyLevel: 'FUSSY',
         size: 'MEDIUM',
-        independenceLevel: 'MEDIUM',
+        independence_level: 'MEDIUM',
+        energy_level: 'FUSSY',
         environment: 'NORMAL',
         images: [''],
         requirement: [''],
         orgId: sub,
       })
 
-    const { id } = newPet.body.newPet.pet as Pet
+    const { petId } = newPet.body
 
-    const response = await request(app.server).get(`/pets/pet/${id}`).send()
+    const response = await request(app.server).get(`/pets/pet/${petId}`).send()
 
     expect(response.statusCode).toEqual(200)
     expect(response.body.pet.name).toEqual('Paçoca')
