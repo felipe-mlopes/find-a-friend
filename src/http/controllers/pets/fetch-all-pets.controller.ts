0db09f6fd1631b328ca3f659415e4a905a469c50
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-29 21:39:42 -0300

    refactor: change searchByCity to fetchAllPetsController function name and makeFetchAllPetsByCityService to makeFetchAllPetsService, update file name and remove city as service params

--- src/http/controllers/pets/fetch-all-pets.controller.ts
+++ src/http/controllers/pets/fetch-all-pets.controller.ts
@@ -1,23 +1,21 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
-import { makeFetchAllPetsByCityService } from '@/services/factories/make-fetch-all-pets-by-city-service'
+import { makeFetchAllPetsService } from '@/services/factories/make-fetch-all-pets-by-city-service'
 
-export async function searchByCity(
+export async function fetchAllPetsController(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
   const fetchAllPetsByCityQuerySchema = z.object({
-    city: z.string(),
     page: z.coerce.number().min(1).default(1),
   })
 
-  const { city, page } = fetchAllPetsByCityQuerySchema.parse(request.query)
+  const { page } = fetchAllPetsByCityQuerySchema.parse(request.query)
 
-  const fecthByCityService = makeFetchAllPetsByCityService()
+  const fecthByCityService = makeFetchAllPetsService()
 
   const { pets } = await fecthByCityService.execute({
-    query: city,
     page,
   })
 
