9f8ab7b8a6dc9ae0f9b35ad2c56ec6d9cc12bb1c
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-07 19:23:45 -0300

    feat: create errorHandler function

---
+++ src/http/controllers/errors.ts
@@ -0,0 +1,52 @@
+import { FastifyError, FastifyReply, FastifyRequest } from 'fastify'
+import { ZodError } from 'zod'
+
+import { ResourceNotFoundError } from '@/services/errors/resource-not-found-error'
+import { OrgAlreadyExistsError } from '@/services/errors/org-already-exists-error'
+import { InvalidCEPError } from '@/services/errors/invalid-cep-error'
+import { InvalidCredentialsError } from '@/services/errors/invalid-credentials-error'
+
+export function errorHandler(
+  error: FastifyError,
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (error instanceof ResourceNotFoundError) {
+    return reply.status(404).send({ message: 'Not found.' })
+  }
+
+  if (error instanceof OrgAlreadyExistsError) {
+    return reply.status(409).send({ message: 'Email already exists.' })
+  }
+
+  if (error instanceof InvalidCEPError) {
+    return reply.status(404).send({ message: 'Invalid CEP.' })
+  }
+
+  if (error instanceof InvalidCredentialsError) {
+    return reply.status(401).send({ message: 'Invalid email or password.' })
+  }
+
+  reply.log.error(
+    {
+      request: {
+        method: request.method,
+        url: request.url,
+        headers: request.headers,
+        body: request.body,
+        query: request.query,
+        params: request.params,
+      },
+      error,
+    },
+    'Unhandled error occurred.',
+  )
+
+  return reply.code(500).send(error.message)
+}
