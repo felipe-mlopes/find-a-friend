43a689db3787c41831e4f89ec11eda8464965d77
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-09 23:01:22 -0300

    sytle: move the create method to last

--- src/repositories/orgs-repositories.ts
+++ src/repositories/orgs-repositories.ts
@@ -1,9 +1,9 @@
 import { Org, Prisma } from '@prisma/client'
 
 export interface OrgsRepository {
-  create(data: Prisma.OrgCreateInput): Promise<Org>
   findById(id: string): Promise<Org | null>
   findByEmail(email: string): Promise<Org | null>
   findByEmailOrName(email: string, name: string): Promise<Org | null>
   findByCity(city: string): Promise<Org[]>
+  create(data: Prisma.OrgCreateInput): Promise<void>
 }
