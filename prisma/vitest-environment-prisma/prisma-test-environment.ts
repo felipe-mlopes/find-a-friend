9727e95d75b23f2e092a14c49cb027eba0145d99
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-11 12:25:24 -0300

    refactor: reverse DATABASE_TEST_URL to DATABASE_URL

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,16 +1,16 @@
-import { randomUUID } from 'node:crypto'
-import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
 import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 
 const prisma = new PrismaClient()
 
-function generateDatabaseTestURL(schema: string) {
-  if (!process.env.DATABASE_TEST_URL) {
-    throw new Error('Please provide a DATABASE_TEST_URL environment variable.')
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
   }
 
-  const url = new URL(process.env.DATABASE_TEST_URL)
+  const url = new URL(process.env.DATABASE_URL)
 
   url.searchParams.set('schema', schema)
 
@@ -21,9 +21,9 @@ export default <Environment>{
   name: 'prisma',
   async setup() {
     const schema = randomUUID()
-    const databaseTestURL = generateDatabaseTestURL(schema)
+    const databaseURL = generateDatabaseURL(schema)
 
-    process.env.DATABASE_TEST_URL = databaseTestURL
+    process.env.DATABASE_URL = databaseURL
 
     execSync('npx prisma migrate deploy')
 
