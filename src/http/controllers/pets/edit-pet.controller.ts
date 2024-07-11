669f35018b13c26806d410c824a4bb10f7a71c61
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-07-10 23:56:56 -0300

    feat: create edit pet controller

---
+++ src/http/controllers/pets/edit-pet.controller.ts
@@ -0,0 +1,42 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeEditPetService } from '@/services/factories/make-edit-pet.service'
+
+export async function editPetController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const editPetParamsSchema = z.object({
+    id: z.string().uuid(),
+  })
+
+  const editPetBodySchema = z.object({
+    data: z.object({
+      name: z.string().min(3).optional(),
+      description: z.string().optional(),
+      age: z.enum(['PUPPY', 'ADULT', 'SENIOR']).optional(),
+      size: z.enum(['SMALL', 'MEDIUM', 'BIG']).optional(),
+      independence_level: z.enum(['LOW', 'MEDIUM', 'HIGH']).optional(),
+      energy_level: z.enum(['CALM', 'PEACEFUL', 'FUSSY']).optional(),
+      environment: z.enum(['TIGHT', 'NORMAL', 'WIDE']).optional(),
+      images: z.string().array().optional(),
+      requirement: z.string().array().optional(),
+    }),
+  })
+
+  const orgId = request.user.sub
+
+  const { id } = editPetParamsSchema.parse(request.params)
+  const { data } = editPetBodySchema.parse(request.body)
+
+  const editPetService = makeEditPetService()
+
+  const { message } = await editPetService.execute({
+    id,
+    data,
+    orgId,
+  })
+
+  return reply.status(204).send({ message })
+}
